e31937e76ada2e8d9413cdbb867f6121f320e0f6
April Ding <apr.ding@gmail.com>
2020-08-01 15:34:52 -0700

    Working moving face & mouth

--- js/paper-script.js
+++ js/paper-script.js
@@ -1,57 +1,81 @@
-var path = new Path();
-var mousePos = view.center/2;
-path.strokeColor = 'black';
-path.fillColor='#EF9050';
+// var face = new Path('M344,4.51c-99.39,24-168,40-216,86s-94,79-92,153-43,99-35,184,95,142,193,144,359-103,413-80,115-54,54-126-17-78-21-153S485-29.49,344,4.51Z');
+// face.position = view.center;
+// face.fillColor = '#EF9050';
+var circle = new Path.Circle(new Point(view.center), 200);
+circle.fillColor = '#80BC9B';
+var points = 4;
+
+var mouth = new Shape.Ellipse({
+    point: view.center - 20 + new Point(0, 80),
+    size: [40, 20],
+    fillColor: 'black'
+});
+var eye1 = new Path.Circle(new Point(view.center + {
+    x:90,
+    y: -50
+}), 50);
+var iris1 = new Path.Circle(new Point(view.center + {
+    x:70,
+    y: -50
+}), 30);
+var eye2 = new Path.Circle(new Point(view.center + {
+    x: -90,
+    y: -50
+}), 50);
+var iris2 = new Path.Circle(new Point(view.center + {
+    x: -70,
+    y: -50
+}), 30);
+eye1.fillColor = 'white';
+eye2.fillColor = 'white';
+iris1.fillColor = 'black';
+iris2.fillColor = 'black';
+
+var pathHeight = 0.5;
 initializePath();
 
-function initializePath(){
+function initializePath() {
 
-	path.add(new Point(mousePos)  + {x:30, y:85});
-	path.add(new Point(mousePos)  + {x:30, y:25});
-	path.add(new Point(mousePos)  + {x:80, y:25});
-	path.add(new Point(mousePos)  + {x:80, y:75});
-	path.closed = true;
 }
 
 function onMouseMove(event) {
-	mousePos = event.point;
-
-
 }
 // Create a copy of the path and move it 100pt to the right:
 function onFrame(event) {
+    if(vol>0){
+            //face.segments[0].point.y += vol*5;
+        mouth.size =[40, 20+vol*400];
+    }
+    console.log(vol);
 
-	if(path.segments[0].point.x != mousePos.x){
-		if(path.segments[0].point.x < mousePos.x){
-			path.segments[0].point.x += 1;
-		}else if(path.segments[0].point.x > mousePos.x){
-			path.segments[0].point.x -= 1;
-		}
-	}else if(path.segments[0].point.y != mousePos.y){
-		if(path.segments[0].point.y < mousePos.y){
-			path.segments[0].point.y += 1;
-		}else if(path.segments[0].point.y > mousePos.y){
-			path.segments[0].point.y -= 1;
-		}
-	}
+    for(var i = 1; i < points; i++){
+        var sinSeed = event.count + (i + i % 10 * 100);
+        var sinHeight = Math.sin(sinSeed / 200) * pathHeight;
+        if(vol == null || vol == 0 || vol == undefined){
+            var xPos = Math.sin(sinSeed / 100) * sinHeight;
+    		var yPos = Math.sin(sinSeed / 100) * sinHeight;
+        }
+        if(vol > 0){
+            var xPos = Math.sin(sinSeed / 100) * sinHeight + vol*2;
+    		var yPos = Math.sin(sinSeed / 100) * sinHeight + vol*2;
+        }
 
+        circle.segments[0].point.x += (xPos/2);
+		circle.segments[1].point.y += (yPos/2);
+        circle.segments[2].point.x += (xPos/2);
+        circle.segments[3].point.y += (yPos/4);
+    }
 
-// Smooth the segments of the copy:
- path.smooth();
+    // Smooth the segments of the copy:
+    circle.smooth();
 
-// Select the path, so we can see its handles:
-	path.fullySelected = true;
+    // Select the path, so we can see its handles:
+    //circle.fullySelected = true;
 
-    var myCircle = new Path.Circle({
-		center: view.center,
-		radius: vol*500
-	});
-	myCircle.strokeColor = 'black';
-	myCircle.fillColor = 'white';
 
 }
 
 function onResize(event) {
-	// Whenever the window is resized, recenter the path:
-	path.position = view.center;
+    // Whenever the window is resized, recenter the path:
+    //path.position = view.center;
 }
